30c2e6c6343e9f256c7484e5e2535b25359970b7
arroz <bet.besa.2003@gmail.com>
2025-11-20 10:14:42 +0100

    ControladorPartida, VistaWordle - inicial

--- src/main/java/Controller/ControladorPartida.java
+++ src/main/java/Controller/ControladorPartida.java
@@ -1,5 +1,17 @@
 package Controller;
 
+import Model.Partida;
+import Model.Diccionari;
+import Model.ResultatIntent;
+import View.VistaWordle;
+
 public class ControladorPartida {
+	
+	private Partida partida;
+	private VistaWordle vista;
+	
+	public ControladorPartida() {}
+	public ControladorPartida( Partida p, VistaWordle v) {}
+	
 
 }
--- src/main/java/View/VistaWordle.java
+++ src/main/java/View/VistaWordle.java
@@ -1,5 +1,16 @@
 package View;
 
-public class VistaWordle {
+import Model.Partida;
+import Model.ResultatIntent;
+import java.util.List;
 
+
+public class VistaWordle {
+	
+	public VistaWordle() {}
+	public void mostrarBenvinguda() {}
+	public  void mostrarTauler(Partida p) {}
+	public void mostrarMissatgeVictoria() {}
+	public void mostrarMissatgeDerrota() {}
+	
 }
